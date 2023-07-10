3bbd8bc67cc7e11335af563e7a3f1cff834e7a7c
Andrei-David Nan <nandavid190@gmail.com>
2023-07-10 10:11:05 +0300

    Initial commit;

---
+++ src/test/java/com/solid/solidbackend/SolidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solid.solidbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolidBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
