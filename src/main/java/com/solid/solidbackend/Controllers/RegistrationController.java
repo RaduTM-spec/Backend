2e415c492df159b44f42b01e9c17e158c6d3da63
Andrei-David Nan <nandavid190@gmail.com>
2023-07-20 10:39:40 +0300

    v0.1.5 refactored controllers to meet feedback from mentors;

--- src/main/java/com/solid/solidbackend/Controllers/RegistrationController.java
+++ src/main/java/com/solid/solidbackend/Controllers/RegistrationController.java
@@ -15,24 +15,19 @@ public class RegistrationController {
 
 
     @PostMapping("/")
-    public ResponseEntity<Optional<User>> authenticateUser(@RequestBody User user) {
+    public ResponseEntity<User> authenticateUser(@RequestBody User user) {
         // Logic to handle an existing user authentication
         // This appears if users selects he is an existing user
-        // Check if username exists in the database
-        // If the user is already registered with a role, return the User object from db(search by username)
-        // If it does not exist, return nothing(research optionals) and put 401 status code
-
+        // We consider that if the user sends this post, 100% he exists in the database (from IBM specs)
+        // So this will always retrun a user obj
         return null;
     }
 
     @PostMapping("/new/member/{activityName}")
-    public ResponseEntity<Optional<User>> addUser(@RequestBody User user,
+    public ResponseEntity<User> addUser(@RequestBody User user,
                                                   @PathVariable String activityName) {
         // Logic to handle new user registration if he selects he is a member
-        // If for some reason the user exists already you should not create the user
-        // !! If activityName DOES NOT exist you should not create the user, because he has no team to join
-        // Return appropriate response, this should almost always return a User object, exception being the case in
-
+        // The user should be considered as non existent
         return null;
     }
 
@@ -41,20 +36,15 @@ public class RegistrationController {
                                                     @PathVariable String activityName,
                                                     @PathVariable String dueDate) {
         // Logic to handle new mentor registration
-        // If for some reason the user exists already you should not create the user
-        // If activityName ALREADY exists you should not create the user
-        // Return appropriate response, this should almost always return a User object
-
         return null;
     }
 
-    @PostMapping("/new/lead/{teamName}")
-    public ResponseEntity<Optional<User>> addLead(@RequestBody User user,
+    @PostMapping("/new/lead/{activityName}")
+    public ResponseEntity<User> addLead(@RequestBody User user,
                                                   @PathVariable String teamName) {
         // Logic to handle new user registration if he is a team lead
-        // If for some reason the user exists already you should not create the user
-        // If teamName already exists you should not create the user
-        // Return appropriate response, this should almost always return a User object
+        // Return appropriate response, this should always return a User object
+        // We consider that neither the teaam name nor the user already exist
 
         return null;
     }
